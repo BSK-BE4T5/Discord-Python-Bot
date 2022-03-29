08308f937e889382f281bba6b7dc192fabb3ffd8
BSK-BE4T5 <panfilodemon1@gmail.com>
2022-03-29 16:23:26 -0600

    master

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import discord
 from discord.ext import commands
 import music
 from music import music_cog
+import os
 
 cogs = [music]
+
+api_key = os.environ.get('Discord_Token')
 
 client = discord.Client()
 client = commands.Bot(command_prefix='+',intents = discord.Intents.all())
@@ -19,4 +22,4 @@ client.add_cog(music_cog(client))
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-client.run('token')
+client.run(api_key)
